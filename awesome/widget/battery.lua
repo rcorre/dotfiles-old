7c1266a0e96c42c4d2687de15307320f047c4725
murphyslaw480 <murphyslaw480@gmail.com>
2013-12-26 14:20:12 -0500

    [awesome] colored battery and temp text

--- awesome/widget/battery.lua
+++ awesome/widget/battery.lua
@@ -2,12 +2,19 @@ local wibox = require("wibox")
 local awful = require("awful")
 local vicious = require("vicious")
 
-battery_widget = awful.widget.progressbar()
-battery_widget:set_width(8)
-battery_widget:set_height(10)
-battery_widget:set_vertical(true)
-battery_widget:set_background_color("#494B4F")
-battery_widget:set_border_color(nil)
-battery_widget:set_color({ type = "linear", from = {0,0}, to = {0,20}, 
-stops = {{0, "#0000FF"}, {0.3, "#00008F"}, {1, "#FF0000"}}})
-vicious.register(battery_widget, vicious.widgets.bat, "$2", 61, "BAT0")
+battery_widget = wibox.widget.textbox()
+local update_function = function (widget, args)
+    charge = args[2]
+    local tag = '<span weight="bold" '
+    if charge < 25 then
+        tag = tag .. 'color="red" size="large">'
+    else if charge < 50 then
+        tag = tag .. 'color="yellow">'
+    else
+        tag = tag .. 'color="cyan">'
+    end
+    return " " .. tag .. 'BAT: ' .. charge .. '%</span> ' end
+end
+
+vicious.register(battery_widget, vicious.widgets.bat, update_function,
+    61, "BAT0")
--- awesome/widget/thermal.lua
+++ awesome/widget/thermal.lua
@@ -1,6 +1,21 @@
 local wibox = require("wibox")
 local vicious = require("vicious")
 
+
 thermal_widget = wibox.widget.textbox()
-vicious.register(thermal_widget, vicious.widgets.thermal, "TEMP: $1°C", 19, 
+
+local update_function = function (widget, args)
+    temp = args[1]
+    local tag = '<span weight="bold" '
+    if temp > 80 then
+        tag = tag .. 'color="red" size="large">'
+    else if temp > 60 then
+        tag = tag .. 'color="yellow">'
+    else
+        tag = tag .. 'color="cyan">'
+    end
+    return " " .. tag .. 'TEMP: ' .. temp .. '°C</span> ' end
+end
+
+vicious.register(thermal_widget, vicious.widgets.thermal, update_function, 19, 
     {"coretemp.0", "core"})
