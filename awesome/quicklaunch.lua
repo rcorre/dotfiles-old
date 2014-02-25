d95dce215b419add39f895b9e8cb82aa46c05992
murphyslaw480 <murphyslaw480@gmail.com>
2014-02-24 22:01:09 -0500

    [awesome] quicklaunch for google calendar and music

--- awesome/quicklaunch.lua
+++ awesome/quicklaunch.lua
@@ -10,5 +10,17 @@ return {
             awful.util.spawn("firefox")
         end,
         info = "firefox"
+    },
+    c = {
+        func = function()
+            awful.util.spawn("firefox --new-window https://www.google.com/calendar/render")
+        end,
+        info = "google calendar"
+    },
+    p = {
+        func = function()
+            awful.util.spawn("firefox --new-window https://play.google.com/music/listen#/now")
+        end,
+        info = "google play music"
     }
 }
