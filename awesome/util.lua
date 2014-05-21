ebee04c1c505d36b16bdc7fb9a2674704e580e6f
Ryan RC <murphyslaw480@gmail.com>
2014-05-21 13:20:51 -0400

    set default terminal based on what is installed

--- awesome/util.lua
+++ awesome/util.lua
@@ -2,9 +2,12 @@ function is_process_running(proc_name)
   local handle = io.popen("pgrep " .. proc_name)
   local output = handle:read("*a")
   handle:close()
-  naughty.notify({ 
-    title = output,
-    text = output ~= ""
-  })
+  return output ~= ""
+end
+
+function program_exists(progname)
+  local handle = io.popen("which " .. progname .. " 2>/dev/null");
+  local output = handle:read("*a")
+  handle:close()
   return output ~= ""
 end
