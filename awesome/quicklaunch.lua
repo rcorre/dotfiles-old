8f91122abc9c4107691a30834fd4f5b085b06ee0
Ryan RC <murphyslaw480@gmail.com>
2014-07-10 16:27:53 -0400

    add inkscape and gimp to awesome quicklaunch

--- awesome/quicklaunch.lua
+++ awesome/quicklaunch.lua
@@ -2,10 +2,10 @@ return {
     m = {
         func = function()
           if (terminal == 'st') then
-            awful.util.spawn_with_shell(terminal .. 
+            awful.util.spawn_with_shell(terminal ..
               " -c mutt -t mutt -e $HOME/dotfiles/mail/start-mutt.sh")
           else
-            awful.util.spawn_with_shell(terminal .. 
+            awful.util.spawn_with_shell(terminal ..
               " -x $HOME/dotfiles/mail/start-mutt.sh")
           end
         end,
@@ -32,6 +32,18 @@ return {
             )
         end,
         info = "google play music"
+    },
+    i = {
+        func = function()
+            awful.util.spawn("inkscape")
+        end,
+        info = "Inkscape"
+    },
+    i = {
+        func = function()
+            awful.util.spawn("gimp")
+        end,
+        info = "gimp"
     },
     s = {
         func = function()
