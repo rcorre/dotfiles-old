6593680dec2439716f0413aacfc415aade8cbbfa
Ryan Roden-Corrent <ryan@rcorre.net>
2017-10-31 12:19:33 -0400

    Fix editor and add bindings to qutebrowser.

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2,7 +2,7 @@
 c = c  # noqa: F821 pylint: disable=invalid-name,undefined-variable
 config = config  # noqa: F821 pylint: disable=invalid-name,undefined-variable
 
-c.editor.command = ['st', 'nvim', "+'set tw=0'", '-f', '{}']
+c.editor.command = ['st', 'nvim', '-c', 'set tw=0', '-f', '{}']
 c.auto_save.session = True
 c.new_instance_open_target = 'window'
 c.completion.shrink = True
@@ -17,5 +17,9 @@ c.fonts.monospace = '"Hack", ' + c.fonts.monospace
 config.bind('zi', 'zoom-in')
 config.bind('zo', 'zoom-out')
 config.bind('gn', 'navigate next')
+config.bind('>', 'navigate next')
 config.bind('gp', 'navigate prev')
+config.bind('<', 'navigate prev')
+config.bind('gh', 'back -t')
 config.bind('ao', 'download open')
+config.bind('td', 'tab-detach')
